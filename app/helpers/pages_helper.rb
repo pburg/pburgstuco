ec323a0b425bde72c9bc9ec09e8e87f336501c1d
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-10-14 17:32:08 -0400

    Use thumbnails in photo gallery
    It helps a lot to... not load the entire large version of the image into
    each thumbnail. This allows each gallery to contain a "thumbs" directory
    containing a smaller version of each file. The thumbnail must have the
    same filename as its large counterpart to be recognized.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,6 +4,10 @@ module PagesHelper
 
     singles = images.each_with_index.map do |f, i|
       filename = File.join("/", "assets", f.split(File::SEPARATOR)[-3..-1])
+      thumbname = File.exists?(thumbs_dir f) \
+                  ? File.join(thumbs_dir filename)
+                  : filename
+
       title = "#{i + 1} of #{images.size}"
       cls = "single#{i.zero? ? " first" : i == images.size - 1 ? " last" : ""}"
 
@@ -11,7 +15,7 @@ module PagesHelper
         content_tag(:a,
                     rel: "lightbox[#{name.downcase.gsub(" ", "-")}]",
                     href: filename) do
-          image_tag filename, alt: title, size: "100x100"
+          image_tag thumbname, alt: title, size: "100x100"
         end
       end
     end
@@ -21,5 +25,11 @@ module PagesHelper
         singles.collect { |s| concat s }
       end
     end
+  end
+
+private
+
+  def thumbs_dir(path)
+    File.join(File.dirname(path), "thumbs", File.basename(path))
   end
 end
