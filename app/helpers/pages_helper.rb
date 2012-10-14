2782caba57bfc1f932ef58f98d857194512ae895
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-10-14 18:16:05 -0400

    Sort photos in galleries by number in filename
    The photos will now be sorted by their number. I also added
    documentation for PagesHelper.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,10 @@
 module PagesHelper
+  # Takes the photo gallery at +dir+, with the name +name+, and creates a
+  # Lightbox 2 group from them. Within +dir+ there may be a directory named
+  # <tt>thumbs</tt> containing the respective thumbnails for each image.
   def lightbox_group(name, dir)
-    images = Dir[File.join(dir, "*.{jpg,JPG,png,PNG,gif,GIF}")]
+    images = Dir[File.join(dir, "*.{jpg,JPG,png,PNG,gif,GIF}")].
+             sort { |a, b| image_name(a).to_i <=> image_name(b).to_i }
 
     singles = images.each_with_index.map do |f, i|
       filename = File.join("/", "assets", f.split(File::SEPARATOR)[-3..-1])
@@ -29,7 +33,19 @@ module PagesHelper
 
 private
 
+  # Returns the basename of an image without its file extension.
+  #
+  #   image_name "/path/to/name_of_image.jpg"  # => "name_of_image"
+  def image_name(image)
+    File.basename image, "*.{jpg,JPG,png,PNG,gif,GIF}"
+  end
+
+  # Takes the image at +path+ and returns the path to the thumbnail for that
+  # image, assuming that the thumbnail has the same filename and resides in
+  # a child directory named <tt>thumbs</tt>.
+  #
+  #   thumbs_dir "/path/to/image.jpg"  # => "/path/to/thumbs/image.jpg"
   def thumbs_dir(path)
-    File.join(File.dirname(path), "thumbs", File.basename(path))
+    File.join File.dirname(path), "thumbs", File.basename(path)
   end
 end
