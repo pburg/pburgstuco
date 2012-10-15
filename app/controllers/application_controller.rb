232612cf55cb3c433b34395715deef9d29d5ddc4
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-10-15 16:21:55 -0400

    Put version number in footer
    Instead of just linking to the GitHub repository, I replaced it with the
    current version number, linking to that version's entry in the change
    log.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_version
+
+  def set_version
+    @version = File.read(Rails.root.join "VERSION").strip
+    @changelog_link = "https://github.com/pburg/pburgstuco/blob/master/" +
+                      "PRERELEASE-CHANGELOG.md##{@version.gsub ".", ""}"
+  end
 end
